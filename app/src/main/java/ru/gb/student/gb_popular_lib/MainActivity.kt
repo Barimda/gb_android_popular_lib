949258ea44ccdc715ba76689b1236c45bbd37bef
Barekaev Dmitry <barekaev@bk.ru>
2023-03-08 20:05:04 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/student/gb_popular_lib/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.gb.student.gb_popular_lib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
